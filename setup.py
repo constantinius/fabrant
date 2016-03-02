6e2e13298ad8a67f9b8a857ccbc1503f11b64697
Fabian Schindler <fabian.schindler.strauss@gmail.com>
2016-03-02 12:14:00 +0100

    Fixing download URL to tarball url

--- setup.py
+++ setup.py
@@ -2,16 +2,20 @@ from setuptools import setup
 
 import fabrant
 
+version = fabrant.__version__
+
 
 setup(
     name='fabrant',
-    version=fabrant.__version__,
+    version=version,
     description="Easy handling of vagrant hosts within fabric",
     long_description=open("README.rst").read(),
     author='Fabian Schindler',
     author_email='fabian.schindler@eox.at',
     license='MIT',
     url='https://github.com/constantinius/fabrant',
-    download_url='https://github.com/constantinius/fabrant/releases',
+    download_url=(
+        'https://github.com/constantinius/fabrant/archive/v%star.gz' % version
+    ),
     py_modules=['fabrant'],
 )
